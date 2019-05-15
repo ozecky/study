3215ca9f147d1a38fcc4aa59368f3ca0716d0986
Yohei Ozeki <ozeki@ataglance.co.jp>
2019-05-15 11:05:19 +0900

    feat(*): modify study_5

--- app/controllers/study_items_controller.rb
+++ app/controllers/study_items_controller.rb
@@ -3,13 +3,15 @@ class StudyItemsController < ApplicationController
     respond_to do |format|
       format.html do
         puts 'format.html'
-        render inline: '<h1>inline</h1>'
       end
       format.js do
         puts 'format.js'
       end
       format.json do
         puts 'format.json'
+      end
+      format.csv do
+        puts 'format.csv'
       end
     end
   end
